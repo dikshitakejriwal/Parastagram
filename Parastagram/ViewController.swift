31ed153caddc24ebe8ea7186e7a3144b9de77ee1
dikshitakejriwal <dikshita.kejriwal@gmail.com>
2021-03-21 11:52:10 -0400

    Initial Commit

---
+++ Parastagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parastagram
+//
+//  Created by dikshita kejriwal on 3/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
