7d149d6b954220588a23792fa1f577e2cccc7bf0
dikshitakejriwal <dikshita.kejriwal@gmail.com>
2021-03-23 22:25:55 -0400

    Parastagram Part 1

--- Parastagram/AppDelegate.swift
+++ Parastagram/AppDelegate.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
-
+import Parse 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+            $0.applicationId = "Ayqbbs2nn8XYhiroQOsuZvdYUUils8Dk5t0P3Eq7"
+            $0.clientKey = "S1ZB18EPElPTbG1AOpzddBevjSpwvKEDJbcAbPxr"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Parastagram/PostCell.swift
@@ -0,0 +1,29 @@
+//
+//  PostCell.swift
+//  Parastagram
+//
+//  Created by dikshita kejriwal on 3/22/21.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
